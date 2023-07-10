3f9b3dd2415ef774bad27fdac567dc07129a5c75
IrynaSoshenko <112086355+IrinaSoshenco@users.noreply.github.com>
2023-07-10 23:38:10 +0200

    Update Main.java
    Test commit 3 IDEA

--- src/main/java/com/ua/hillel/soshenko/lessons/lesson4/Main.java
+++ src/main/java/com/ua/hillel/soshenko/lessons/lesson4/Main.java
@@ -2,8 +2,6 @@ package com.ua.hillel.soshenko.lessons.lesson4;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
-        System.out.println("Hello Git");
-        System.out.println("Hello Git");
+        System.out.println("Hello GitHUB");
     }
 }
